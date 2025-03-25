b43068fda55fbe6521ff37a863dc51f000277f60
IvanSkobov <LOC-DOC@LIST.RU>
2025-03-25 14:13:08 +0400

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,35 @@ window.geometry("300x300")
 concurrent_player = "x"
 buttons = []
 
-def on_click(row, col):
-    pass
+def check_winner():
+    for i in range(3):
+        if buttons[i][0]['text'] == buttons[i][1]['text'] == buttons[i][2]['text'] !="":
+            return True
+        if buttons[0][i]['text'] == buttons[1][i]['text'] == buttons[2][i]['text'] !="":
+            return True
 
+    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] !="":
+        return True
+    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] !="":
+           return True
+    return False
+
+
+
+
+def on_click(row, col):
+    global concurrent_player
+
+    if buttons[row][col]['text'] != "":
+        return
+
+    buttons[row][col]['text'] = concurrent_player
+
+    if check_winner():
+        messagebox.showinfo(f"Игра-окончена", f"Игрок {concurrent_player } победил!!!")
+
+
+    concurrent_player = "0" if concurrent_player == "x" else "x"
 for i in range(3):
     row = []
     for j in range(3):
